d44c445389ce56af9a0e8cdb9437835533825c09
Konrad Pietralik <hidden@example.com>
2023-09-13 16:32:50 +0200

    Remembering last picked day of week and hide completed

--- Amrap/Consts.cs
+++ Amrap/Consts.cs
@@ -2,7 +2,8 @@
 
 internal static class Consts
 {
+    internal const string FilterDayKey = "net.yius.amrep.filter_day";
+    internal const string HideCompletedKey = "net.yius.amrep.hide_completed";
     internal const string ExerciseTypesUrlKey = "net.yius.amrep.exercise_types_url";
     internal const string WorkoutPlanUrlKey = "net.yius.amrep.workout_plan_url";
-    internal const string Migration1AppliedKey = "net.yius.amrep.migration.1";
 }
